bb993b292a61c3b4039cfa76aa9e4c6a43c850c5
Vitalii Kulanov <vitaliy@kulanov.org.ua>
2017-12-16 08:20:53 +0200

    Get rid of debug entry point
    This patch makes several changes:
    * removes debug entry point
    * uses the latest version of 'cliff' from github

--- dropme/app.py
+++ dropme/app.py
@@ -18,6 +18,14 @@ class DropboxClient(app.App):
     Initialization of the command manager and configuration of basic engines.
     """
 
+    def __init__(self):
+        super(DropboxClient, self).__init__(
+            description='CLI tool for managing Dropbox environment.',
+            version='0.0.1',
+            command_manager=CommandManager('dropme', convert_underscores=True),
+            deferred_help=True
+            )
+
     def build_option_parser(self, description, version, argparse_kwargs=None):
         option_parser = super(DropboxClient, self).build_option_parser(
             description, version, argparse_kwargs=argparse_kwargs)
@@ -37,27 +45,5 @@ class DropboxClient(app.App):
 
 
 def main(argv=sys.argv[1:]):
-    dropboxclient_app = DropboxClient(
-        description='CLI tool for managing Dropbox environment.',
-        version='0.0.1',
-        command_manager=CommandManager('dropme', convert_underscores=True),
-        deferred_help=True
-    )
+    dropboxclient_app = DropboxClient()
     return dropboxclient_app.run(argv)
-
-
-def debug(name, cmd_class, argv=None):  # pragma: no cover
-    """Helper for debugging single command without package installation."""
-    import sys
-
-    if argv is None:
-        argv = sys.argv[1:]
-
-    argv = [name] + argv + ["-v", "-v", "--debug"]
-    cmd_mgr = CommandManager("test_dropme", convert_underscores=True)
-    cmd_mgr.add_command(name, cmd_class)
-    return DropboxClient(
-        description="CLI tool for managing Dropbox environment.",
-        version="0.0.1",
-        command_manager=cmd_mgr
-    ).run(argv)
